e663ad3739d0176967fbc904e8c46cf610ce4724
martin-tresor <tresor19mbang@gmail.com>
2018-12-20 12:12:30 +0100

    creation du controlleur annotationAdvice qui regroupe toutes les variables partagees du contexte de thymeleaf

--- src/main/java/cm/rst/controller/AnnotationAdvice.java
+++ src/main/java/cm/rst/controller/AnnotationAdvice.java
@@ -1,7 +1,29 @@
 package cm.rst.controller;
 
+import cm.rst.entities.ProduitCommande;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
 /**
  * Created by Martins on 20/12/2018.
  */
+
+@ControllerAdvice
 public class AnnotationAdvice {
+    @ModelAttribute("quantite")
+    public int Quantite(HttpSession session) {
+        List<ProduitCommande> panier = (List<ProduitCommande>) session.getAttribute("panier");
+        int quantite = 0;
+        if (session.getAttribute("panier") == null) {
+            quantite = 0;
+        }else {
+            for (int i=0; i<panier.size(); i++) {
+                quantite += panier.get(i).getQuantiteCmd();
+            }
+        }
+        return quantite;
+    }
 }
